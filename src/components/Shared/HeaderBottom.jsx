e24c6d03cf2f59d37f51a1a19d7c50fa7bf9aa6f
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2023-03-01 20:29:26 +0600

    user image set

--- src/components/Shared/HeaderBottom.jsx
+++ src/components/Shared/HeaderBottom.jsx
@@ -70,7 +70,7 @@ const HeaderBottom = () => {
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-cute-boy-face-cartoon-vector-illustration-graphic-design-110654225.jpg" alt='profile' />
+                                <img src="https://i.ibb.co/sqLpJP5/cute-boy-face-cartoon-cute-boy-face-cartoon-vector-illustration-graphic-design-110654225.jpg" alt='profile' />
                             </div>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
