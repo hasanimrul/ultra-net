869fb4a63ecb3f8abd4674883270f59f8d80c613
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2023-03-02 21:58:13 +0600

    some modification

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { AiOutlineWifi } from 'react-icons/ai';
 import img from '../../assets/internet.jpg'
 
+
 const Banner = () => {
     return (
         <div className='bg-cover bg-top pt-32 pb-14' style={{ backgroundImage: `url(${img})` }}>
             <div className='ml-16'>
                 <p className='flex items-center gap-3'><AiOutlineWifi className='text-purple-900 text-xl' />Welcome to UltraNet</p>
-                <h3 className=' text-left text-5xl font-bold leading-normal'>Connect Faster And <br /> Easier With <span className='text-purple-900'>Ultra</span>Net.</h3>
+                <h3 className=' text-left text-5xl font-bold leading-normal' >Connect Faster And <br /> Easier With <span className='text-purple-900'>Ultra</span>Net.</h3>
             </div>
             <div className='ml-28 mt-36'>
                 <h3 className=' text-left text-xl '>GET A FASTER FIBER DEAL FROM ONLY-</h3>
